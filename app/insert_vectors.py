3e0afb039b6a0b003d29ad91b4ba0bfe855590b3
Marco Carnaghi <carnaghi.marco@gmail.com>
2024-10-21 08:15:57 -0300

    env settings

--- app/insert_vectors.py
+++ app/insert_vectors.py
@@ -4,12 +4,42 @@ import pandas as pd
 from database.vector_store import VectorStore
 from timescale_vector.client import uuid_from_time
 
+from sentence_transformers import SentenceTransformer
+import logging
+import time
+from typing import Any, List, Optional, Tuple, Union
+
 # Initialize VectorStore
 vec = VectorStore()
 
 # Read the CSV file
-df = pd.read_csv("../data/faq_dataset.csv", sep=";")
+df = pd.read_csv( '../data/Diccionario_Conceptos.csv', delimiter=';')
+# Add the new column 'categoria' with the same value for all rows
+df['categoria'] = 'Diccionario de Conceptos'
 
+
+
+def get_embedding_model():
+  model = SentenceTransformer('all-MiniLM-L6-v2')
+  return model
+
+def get_embedding(text: str) -> List[float]:
+    """
+    Generate embedding for the given text.
+
+    Args:
+        text: The input text to generate an embedding for.
+
+    Returns:
+        A list of floats representing the embedding.
+    """
+    text = text.replace("\n", " ")
+    start_time = time.time()
+    model = get_embedding_model()
+    embedding = model.encode(text)
+    elapsed_time = time.time() - start_time
+    logging.info(f"Embedding generated in {elapsed_time:.3f} seconds")
+    return embedding
 
 # Prepare data for insertion
 def prepare_record(row):
@@ -32,13 +62,14 @@ def prepare_record(row):
 
         This is useful when your content already has an associated datetime.
     """
-    content = f"Question: {row['question']}\nAnswer: {row['answer']}"
-    embedding = vec.get_embedding(content)
+    content = f"Concepto: {row['concepto ']}\definicion: {row[' definicion ']}\nEjemplo: {row[' ejemplo']}"
+    #embedding = vec.get_embedding(content)
+    embedding = get_embedding(content)
     return pd.Series(
         {
-            "id": str(uuid_from_time(datetime.now())),
+            "id": 'DicConceptos' + str(uuid_from_time(datetime.now())),
             "metadata": {
-                "category": row["category"],
+                "category": row["categoria"],
                 "created_at": datetime.now().isoformat(),
             },
             "contents": content,
@@ -50,6 +81,6 @@ def prepare_record(row):
 records_df = df.apply(prepare_record, axis=1)
 
 # Create tables and insert data
-vec.create_tables()
-vec.create_index()  # DiskAnnIndex
-vec.upsert(records_df)
+#vec.create_tables()
+#vec.create_index()  # DiskAnnIndex
+#vec.upsert(records_df)
